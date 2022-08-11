d6c64924cbe0dd5deceb09924bfc7fdfd44d9ce2
gtithathonor <jeong.suyoung92@gmail.com>
2022-08-11 21:40:50 +0900

    입출력 - 10951 풀이중

---
+++ Backjoon/src/io/num/Main10951.java
@@ -0,0 +1,14 @@
+package io.num;
+
+import java.util.Scanner;
+
+public class Main10951 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
