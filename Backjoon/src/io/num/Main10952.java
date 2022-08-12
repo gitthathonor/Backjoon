26a1e33e4e48d2d70ac16fe6101cf2ed64155bb8
gtithathonor <jeong.suyoung92@gmail.com>
2022-08-12 21:40:21 +0900

    입출력 - 10950,10952 완료/10953 진행중

--- Backjoon/src/io/num/Main10952.java
+++ Backjoon/src/io/num/Main10952.java
@@ -2,16 +2,19 @@ package io.num;
 
 import java.util.Scanner;
 
-public class Main10951 {
+public class Main10952 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a,b;
-		
 		while(sc.hasNextInt()) {
 			a = sc.nextInt();
 			b = sc.nextInt();
+			if(a == 0 && b == 0) {
+				break;
+			}
 			System.out.println(a+b);
+			
 		}
 		
 		sc.close();
---
+++ Backjoon/src/io/num/Main10953.java
@@ -0,0 +1,28 @@
+package io.num;
+
+import java.util.Scanner;
+
+public class Main10953 {
+	public static void main(String[] args) {
+		String s = "5\n1,1\n2,3\n3,4\n9,8\n5,2\n";
+		Scanner sc = new Scanner(s);
+		System.out.println(s.substring(0, 1));
+		String input = s.substring(2);
+		System.out.println(input);
+//		String[] inputArr = input.split(",");
+//		for(String str : inputArr) {
+//			System.out.println(str);
+//		}
+//		System.out.println(input);
+//		int length = Integer.parseInt(s.substring(0, 1));
+//		int[] a = new int[length];
+//		int[] b = new int[length];
+		
+//		while(sc.hasNext()) {
+//			for(int i=0; i<length; i++) {
+//				a[i] = 
+//			}
+//		}
+		System.out.println(s);
+	}
+}
