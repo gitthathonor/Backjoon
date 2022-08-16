b91ed2aa8021a22ed5ef0f827bc45c8bbfdf9422
gtithathonor <jeong.suyoung92@gmail.com>
2022-08-16 18:22:45 +0900

    입출력 - 10953해결, 11021풀이중

--- Backjoon/src/io/num/Main10953.java
+++ Backjoon/src/io/num/Main10953.java
@@ -4,31 +4,11 @@ import java.util.Scanner;
 
 public class Main10953 {
 	public static void main(String[] args) {
-		String s = "5\n1,1\n2,3\n3,4\n9,8\n5,2\n";
-		Scanner sc = new Scanner(s);
-		System.out.println(s.substring(0, 1));
-		int total = Integer.parseInt(s.substring(0, 1));
-		String input = s.substring(2);
-		String[] inputs = input.split("\n");
-		String[] inputsA = new String[total];
-		String[] inputsB = new String[total];
-		
-		for(int i = 0; i<inputs.length; i++) {
-			
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			String[] arr = sc.next().split(",");
+			System.out.println(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
 		}
-		
-		
-//		int[] a = new int[total];
-//		int[] b = new int[total];
-//		for(int i=0; i<total; i++) {
-//			a[i] = 
-//		}
-		
-		
-		
-		// System.out.println(input);
-		
-		
-		// System.out.println(s);
 	}
 }
---
+++ Backjoon/src/io/num/Main11021.java
@@ -0,0 +1,26 @@
+//package io.num;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class Main11021 {
+//
+//	public static void main(String[] args) {
+//		try {
+//			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//			String str;
+//			StringTokenizer st;
+//			
+//			int t = Integer.parseInt(br.readLine()); // 테스트 케이스 갯수
+//			
+//			
+//			
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//
+//}
